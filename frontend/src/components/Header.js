f98b662dcca9bd31f71eba086e598f208f352657
joshu <joshua.v.dev@gmail.com>
2020-11-17 16:14:39 -0500

    changed name to Off Set Designs

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -8,7 +8,7 @@ const Header = () => {
       <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
         <Container>
           <LinkContainer to="/">
-            <Navbar.Brand>Biotix Wellness</Navbar.Brand>
+            <Navbar.Brand>Off Set Designs</Navbar.Brand>
           </LinkContainer>
 
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
