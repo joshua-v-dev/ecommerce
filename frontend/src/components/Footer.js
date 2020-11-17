7cfe4c79bb1771e9655a284b9ae135c9d91982de
joshu <joshua.v.dev@gmail.com>
2020-11-17 11:50:11 -0500

    header with bootstrap & footer setup

---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {Container, Row, Col} from 'react-bootstrap';
+
+const Footer = () => {
+  return (
+    <Container>
+      <Row>
+        <Col className="text-center py-3">Copyright &copy; Biotix Wellness</Col>
+        <footer>Footer</footer>;
+      </Row>
+    </Container>
+  );
+};
+
+export default Footer;
