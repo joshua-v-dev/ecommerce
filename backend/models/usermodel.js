e9955dbd242c9556bd50f61ee1f223865164d621
joshu <joshua.v.dev@gmail.com>
2020-11-19 13:40:29 -0500

    set schema for productmodel

--- backend/models/usermodel.js
+++ backend/models/usermodel.js
@@ -3,10 +3,10 @@ import mongoose from 'mongoose';
 const userSchema = mongoose.Schema(
   {
     name: {
-      type: {String, required: true, unique: true},
+      type: {String, required: true},
     },
     email: {
-      type: {String, required: true},
+      type: {String, required: true, unique: true},
     },
     password: {
       type: {String, required: true},
