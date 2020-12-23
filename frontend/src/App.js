a98eca640a2a56c5129dd291ada3e6ad53231539
joshu <joshua.v.dev@gmail.com>
2020-12-23 11:56:47 -0500

    add admin dashboard and functionality

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -14,6 +14,7 @@ import ShippingScreen from './screens/ShippingScreen';
 import PaymentScreen from './screens/PaymentScreen';
 import PlaceOrderScreen from './screens/PlaceOrderScreen';
 import OrderScreen from './screens/OrderScreen';
+import UserListScreen from './screens/UserListScreen';
 const App = () => {
   return (
     <Router>
@@ -30,6 +31,7 @@ const App = () => {
           <Route path="/product/:id" component={ProductScreen} />
           <Route path="/cart/:id?" component={CartScreen} />
           <Route path="/" component={HomeScreen} exact />
+          <Route path="/adminuserlist" component={UserListScreen} exact />
         </Container>
       </main>
       <Footer />
