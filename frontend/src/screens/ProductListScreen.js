e4e3514cf47ea7eccce683a3e29d02b1608a9a14
joshu <joshua.v.dev@gmail.com>
2021-01-01 14:01:52 -0500

    add delete product functionality front and back

--- frontend/src/screens/ProductListScreen.js
+++ frontend/src/screens/ProductListScreen.js
@@ -4,11 +4,18 @@ import {Table, Button, Row, Col} from 'react-bootstrap';
 import {useDispatch, useSelector} from 'react-redux';
 import Message from '../components/Message';
 import Loader from '../components/Loader';
-import {listProducts} from '../actions/productActions';
+import {listProducts, deleteProduct} from '../actions/productActions';
 const ProductListScreen = ({history, match}) => {
   const dispatch = useDispatch();
   const productList = useSelector((state) => state.productList);
   const {loading, error, products} = productList;
+
+  const productDelete = useSelector((state) => state.productDelete);
+  const {
+    loading: loadingDelete,
+    error: errorDelete,
+    success: successDelete,
+  } = productDelete;
 
   const userLogin = useSelector((state) => state.userLogin);
   const {userInfo} = userLogin;
@@ -19,11 +26,11 @@ const ProductListScreen = ({history, match}) => {
     } else {
       history.push('/login');
     }
-  }, [dispatch, history, userInfo]);
+  }, [dispatch, history, userInfo, successDelete]);
 
   const deleteHandler = (id) => {
     if (window.confirm('Are you sure')) {
-      //  Delete Product
+      dispatch(deleteProduct(id));
     }
   };
   const createProductHandler = (product) => {
@@ -41,7 +48,8 @@ const ProductListScreen = ({history, match}) => {
           </Button>
         </Col>
       </Row>
-
+      {loadingDelete && <Loader />}
+      {errorDelete && <Message variant="danger">{errorDelete}</Message>}
       {loading ? (
         <Loader />
       ) : error ? (
