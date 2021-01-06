a70f449acd9ee66b270b2b675bbcd7dd4d8ea795
joshu <joshua.v.dev@gmail.com>
2021-01-06 18:19:59 -0500

    add multer dep,edit/create product screen and func

--- backend/Routes/orderRoutes.js
+++ backend/Routes/orderRoutes.js
@@ -8,7 +8,7 @@ import {
 } from '../controllers/orderController.js';
 import {protect} from '../middleware/authMiddleware.js';
 
-router.route('/').post(protect, addOrderItems);
+router.route('/').post(protect, addOrderItems).get(protect, admin, getMyOrders);
 router.route('/myorders').get(protect, getMyOrders);
 router.route('/:id').get(protect, getOrderById);
 router.route('/:id/pay').put(protect, updateOrderToPaid);
--- frontend/src/screens/ProductEditScreen.js
+++ frontend/src/screens/ProductEditScreen.js
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import React, {useState, useEffect} from 'react';
 import {Link} from 'react-router-dom';
 import {Form, Button} from 'react-bootstrap';
@@ -5,7 +6,7 @@ import {useDispatch, useSelector} from 'react-redux';
 import Message from '../components/Message';
 import Loader from '../components/Loader';
 import FormContainer from '../components/FormContainer';
-import {listProductDetails} from '../actions/productActions';
+import {listProductDetails, updateProduct} from '../actions/productActions';
 import {PRODUCT_UPDATE_RESET} from '../constants/productConstants';
 
 const ProductEditScreen = ({match, history}) => {
@@ -18,6 +19,7 @@ const ProductEditScreen = ({match, history}) => {
   const [category, setCategory] = useState('');
   const [countInStock, setCountInStock] = useState('');
   const [description, setDescription] = useState('');
+  const [uploading, setUploading] = useState(false);
 
   const dispatch = useDispatch();
 
@@ -50,10 +52,32 @@ const ProductEditScreen = ({match, history}) => {
     }
   }, [dispatch, history, productId, product, successUpdate]);
 
+  const uploadFileHandler = async (e) => {
+    const file = e.target.files[0];
+    const formData = new FormData();
+    formData.append('image', file);
+    setUploading(true);
+
+    try {
+      const config = {
+        headers: {
+          'Content-Type': 'multipart/form-data',
+        },
+      };
+      const {data} = await axios.post('/api/upload', formData, config);
+
+      setImage(data);
+      setUploading(false);
+    } catch (error) {
+      console.error(error);
+      setUploading(false);
+    }
+  };
+
   const submitHandler = (e) => {
     e.preventDefault();
     dispatch(
-      productUpdate({
+      updateProduct({
         _id: productId,
         name,
         price,
@@ -106,6 +130,12 @@ const ProductEditScreen = ({match, history}) => {
                 placeholder="Enter image URL"
                 value={image}
                 onChange={(e) => setImage(e.target.value)}></Form.Control>
+              <Form.File
+                id="image-file"
+                label="Choose File"
+                custom
+                onChange={uploadFileHandler}></Form.File>
+              {uploading && <Loader />}
             </Form.Group>
 
             <Form.Group controlId="brand">
