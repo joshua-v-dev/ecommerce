43b7ccd090254b8f7227f89189e1cc6ddf1ff9e9
joshu <joshua.v.dev@gmail.com>
2021-01-09 06:23:26 -0500

    add orderDelivered button and functionality bk/ft

--- backend/Routes/orderRoutes.js
+++ backend/Routes/orderRoutes.js
@@ -4,6 +4,7 @@ import {
   addOrderItems,
   getOrderById,
   updateOrderToPaid,
+  updateOrderToDelivered,
   getMyOrders,
 } from '../controllers/orderController.js';
 import {protect, admin} from '../middleware/authMiddleware.js';
@@ -12,5 +13,6 @@ router.route('/').post(protect, addOrderItems).get(protect, admin, getMyOrders);
 router.route('/myorders').get(protect, getMyOrders);
 router.route('/:id').get(protect, getOrderById);
 router.route('/:id/pay').put(protect, updateOrderToPaid);
+router.route('/:id/deliver').put(protect, admin, updateOrderToDelivered);
 
 export default router;
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -24,6 +24,7 @@ import {
   orderPayReducer,
   orderListMyReducer,
   orderListReducer,
+  orderDeliveredReducer,
 } from './reducers/orderReducers';
 
 const reducer = combineReducers({
@@ -43,6 +44,7 @@ const reducer = combineReducers({
   orderCreate: orderCreateReducer,
   orderDetails: orderDetailsReducer,
   orderPay: orderPayReducer,
+  orderDeliver: orderDeliveredReducer,
   orderListMy: orderListMyReducer,
   orderList: orderListReducer,
 });
